eab6f5707628905904e342d3ec0addd8a09b5c4e
James deBoer <james@huronbox.com>
2013-09-24 10:27:25 -0700

    Disable most of the versions in karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -9,12 +9,12 @@ module.exports = function(config) {
     // optionally 'watched' only.
     files: [
       'benchmark.js',
-      '../build/angular.js',
-      'capture/capture-latest.js',
+      //'../build/angular.js',
+      //'capture/capture-latest.js',
       'builds/angular-1.0.0rc8.js',
       'capture/capture-1.0.0rc8.js',
-      'builds/angular-1.0.0rc1.js',
-      'capture/capture-1.0.0rc1.js',
+      //'builds/angular-1.0.0rc1.js',
+      //'capture/capture-1.0.0rc1.js',
     
       'specs/*.js', // test ngBench before any tests are run!
     
