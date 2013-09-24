b2e6621a1ce6fbefa5416cbdf3dd1b7ac0f7b900
James deBoer <james@huronbox.com>
2013-09-23 20:20:47 -0700

    chore(ngBench): Upgrade to karma

---
+++ ngBench/karma.conf.js
@@ -0,0 +1,33 @@
+module.exports = function(config) {
+  config.set({
+    //logLevel: config.LOG_DEBUG,
+    basePath: '.',
+    frameworks: ['jasmine'],
+
+    // list of files / patterns to load in the browser
+    // all tests must be 'included', but all other libraries must be 'served' and
+    // optionally 'watched' only.
+    files: [
+      'benchmark.js',
+      '../build/angular.js',
+      'capture/capture-latest.js',
+      'builds/angular-1.0.0rc8.js',
+      'capture/capture-1.0.0rc8.js',
+      'builds/angular-1.0.0rc1.js',
+      'capture/capture-1.0.0rc1.js',
+    
+      'specs/*.js', // test ngBench before any tests are run!
+    
+      'perfs/ng/*.js'
+    ],
+
+    autoWatch: false,
+
+    plugins: ['karma-chrome-launcher', 'karma-jasmine'],
+
+    browsers: ['Chrome'],
+
+    // If browser does not capture in given timeout [ms], kill it
+    captureTimeout: 5000,
+  });
+};
