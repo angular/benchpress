65496d76d452d6748edfe3fd7c03cb61c31b001a
Jeff Cross <middlefloor@gmail.com>
2013-10-01 11:41:37 -0700

    feat(checkout): Added step to checkout SHA in repo.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,34 +1,33 @@
 module.exports = function (grunt) {
-  var q = require('q');
+  var q = require('q'),
+      path = require('path');
 
   grunt.registerTask('compareShas', 'Run tests against two SHAs', function (sha1, sha2) {
-    console.log('comparing', sha1, sha2);
     var done = this.async();
 
     stepThrough(sha1);
 
     function stepThrough (sha) {
       checkForClone(sha)
-        .then(checkoutSHA)
-//        .then(packageBuild)
-//        .then(copyBuild)
-//        .then(generateKarma)
-//        .then(repeat);
+      .then(checkoutSHA)
+      .then(packageBuild)
+      //        .then(copyBuild)
+      //        .then(generateKarma)
+      //        .then(repeat);
     }
 
-    //Step 1: Check to see if a private copy of the repo exists
-   function checkForClone (sha) {
-     grunt.log.writeln('checkForClone');
+    function checkForClone (sha) {
       var deferred = q.defer();
+      grunt.log.writeln('Step 1: Check to see if a private copy of the repo exists');
 
       process.nextTick(function () {
-        grunt.log.writeln('nextTick');
         if (!grunt.file.isDir('angular.js')) {
           grunt.log.writeln('Creating clone of angular.js repo');
           grunt.util.spawn({
             cmd: 'git',
             args: ['clone', 'https://github.com/angular/angular.js']
-          }, function () {
+          },
+          function () {
             deferred.resolve(sha);
           });
         }
@@ -41,24 +40,60 @@ module.exports = function (grunt) {
       return deferred.promise;
     }
 
-    //Step 2: Check out sha in private copy
-
-
     //Step 3: Package the build
     //Step 4: Copy the build into a temporary directory
     //Step 5: Generate a karma config for testing this sha
     //Repeat process for 2nd sha
     //Should this run the tests?
-    //
+    //Should this help set up remotes?
 
     function checkoutSHA (sha) {
-//      grunt.util.spawn({}, packageBuild);
-      grunt.log.writeln('checkout sha ', sha);
-      done();
+      var remote, fullSHA,
+          deferred = q.defer(),
+          repoPath = path.resolve('angular.js');
+
+      if (sha.indexOf('/') !== -1) {
+        remote = sha.split('/')[0];
+        fullSHA = sha.split('/')[1];
+      }
+      else {
+        fullSHA = sha;
+        remote = 'origin';
+      }
+
+      grunt.log.writeln('Step 2: Preparing to checkout sha', sha, 'to build a copy');
+
+      process.nextTick(function () {
+        grunt.log.writeln('Fetching remote...');
+        grunt.util.spawn({cmd: 'cd', args: ['angular.js']}, function () {
+          grunt.log.writeln('directory', process.cwd());
+          grunt.util.spawn({
+            cmd: 'git',
+            args: ['fetch', remote]
+          },
+          function () {
+            grunt.log.writeln('Checking out SHA', fullSHA);
+            grunt.util.spawn({
+              cmd: 'git',
+              args: ['checkout', fullSHA],
+                opts: {cwd: repoPath}
+            },
+            function (err, result, code) {
+              grunt.log.writeln('Done checkout out', err, result, code);
+              grunt.util.spawn({cmd: 'cd', args: ['../']}, function () {
+                deferred.resolve(sha);
+              });
+            });
+          });
+        });
+      });
+
+      return deferred.promise;
     }
 
     function packageBuild (sha) {
-
+      grunt.log.writeln('package build');
+      done();
     }
 
     function copyBuild (sha) {
@@ -66,7 +101,6 @@ module.exports = function (grunt) {
     }
 
     function generateKarma (sha) {
- 
     }
 
     function repeat (sha) {
