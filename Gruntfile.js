08944f395f529ae649f28a4af072a854dd9a61b9
Jeff Cross <middlefloor@gmail.com>
2013-10-02 12:09:48 -0700

    feat(package): Added step to package project for testing.

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,6 +4,7 @@ module.exports = function (grunt) {
       exec = require('child_process').exec;
 
   grunt.registerTask('compareShas', 'Run tests against two SHAs', function (sha1, sha2) {
+    var GIT_FOLDER = 'angular.js';
     var done = this.async();
 
     stepThrough(sha1);
@@ -12,7 +13,7 @@ module.exports = function (grunt) {
       checkForClone(sha)
       .then(checkoutSHA)
       .then(packageBuild)
-      //        .then(copyBuild)
+      .then(copyBuild)
       //        .then(generateKarma)
       //        .then(repeat);
     }
@@ -22,11 +23,11 @@ module.exports = function (grunt) {
       grunt.log.writeln('Step 1: Check to see if a private copy of the repo exists');
 
       process.nextTick(function () {
-        if (!grunt.file.isDir('angular.js')) {
+        if (!grunt.file.isDir(GIT_FOLDER)) {
           grunt.log.writeln('Creating clone of angular.js repo');
           grunt.util.spawn({
-            cmd: 'git',
-            args: ['clone', 'https://github.com/angular/angular.js']
+cmd: 'git',
+              args: ['clone', 'https://github.com/angular/angular.js']
           },
           function () {
             deferred.resolve(sha);
@@ -51,7 +52,7 @@ module.exports = function (grunt) {
     function checkoutSHA (sha) {
       var remote, fullSHA,
           deferred = q.defer(),
-          repoPath = path.resolve('angular.js');
+          repoPath = path.resolve(GIT_FOLDER);
 
       if (sha.indexOf('/') !== -1) {
         remote = sha.split('/')[0];
@@ -66,25 +67,52 @@ module.exports = function (grunt) {
 
       process.nextTick(function () {
         grunt.log.writeln('Fetching remote...');
-        grunt.util.spawn({cmd: 'cd', args: ['angular.js']}, function () {
-          grunt.log.writeln('directory', process.cwd());
-          grunt.util.spawn({
+        grunt.util.spawn({
             cmd: 'git',
-            args: ['fetch', remote]
+            args: ['fetch', remote],
+            opts: {cwd: repoPath}
           },
           function () {
             grunt.log.writeln('Checking out SHA', fullSHA);
             grunt.util.spawn({
               cmd: 'git',
               args: ['checkout', fullSHA],
-                opts: {cwd: repoPath}
+              opts: {cwd: repoPath}
             },
             function (err, result, code) {
-              grunt.log.writeln('Done checkout out', err, result, code);
-              grunt.util.spawn({cmd: 'cd', args: ['../']}, function () {
-                deferred.resolve(sha);
-              });
+              grunt.log.writeln('Done checkout: ', err, result, code);
+              deferred.resolve(sha);
             });
+          });
+      });
+
+      return deferred.promise;
+    }
+
+    function packageBuild (sha) {
+      var deferred = q.defer();
+
+      grunt.log.writeln('Step 3: Package build');
+
+      process.nextTick(function () {
+        //Important if it's a fresh clone. 
+        //Otherwise the package process would
+        //Automatically npm install
+        grunt.log.writeln('Installing npm dependencies...');
+        grunt.util.spawn({
+          cmd: 'npm',
+          args: ['install'],
+          opts: {cwd: path.resolve(GIT_FOLDER)}
+        }, 
+        function () {
+          grunt.log.writeln('Running grunt package...');
+          grunt.util.spawn({
+            cmd: 'grunt',
+            args: ['package'],
+            opts: {cwd: path.resolve(GIT_FOLDER)}
+          },
+          function () {
+            deferred.resolve(sha);
           });
         });
       });
@@ -92,13 +120,9 @@ module.exports = function (grunt) {
       return deferred.promise;
     }
 
-    function packageBuild (sha) {
-      grunt.log.writeln('package build');
-      done();
-    }
-
     function copyBuild (sha) {
-
+      grunt.log.writeln('Step 4: Copy the build to a new directory.');
+      done();
     }
 
     function generateKarma (sha) {
