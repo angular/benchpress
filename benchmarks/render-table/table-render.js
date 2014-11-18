7e0c37591bd93996f1d94a57deabd17c3f756987
Jeff Cross <middlefloor@gmail.com>
2014-11-18 15:18:59 -0800

    fix(example): fix markup and add variables to render-table

--- benchmarks/render-table/table-render.js
+++ benchmarks/render-table/table-render.js
@@ -1,5 +1,18 @@
 (function() {
   var element = document.querySelector('#tableContainer');
+  bp.variables.addMany([{
+    value: 'baseline'
+  },{
+    value: 'colorful'
+  }]);
+
+  window.benchmarkSteps.push({
+    name: 'cleanup',
+    fn: function() {
+      element.innerHTML = '';
+    }
+  });
+
   window.benchmarkSteps.push({
     name: 'table renderer',
     description: 'Create a table with 1000 rows and 20 columns',
@@ -13,16 +26,16 @@
         for (var j=0; j<20; j++) {
           td = document.createElement('td');
           td.innerText = '' + i + j;
+          if (bp.variables.selected && bp.variables.selected.value === 'colorful') {
+            td.style.backgroundColor =
+              'rgb(' +
+              Math.round(Math.random() * 255) + ', ' +
+              Math.round(Math.random() * 255) + ', ' +
+              Math.round(Math.random() * 255) + ')';
+          }
           tr.appendChild(td);
         }
       }
-    }
-  });
-
-  window.benchmarkSteps.push({
-    name: 'cleanup',
-    fn: function() {
-      element.innerHtml = '';
     }
   });
 })();
