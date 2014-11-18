e74e628e5d4d99163378afb33fe0aad10c722772
Jeff Cross <middlefloor@gmail.com>
2014-11-18 10:01:46 -0800

    fix(RunState): change sample and iteration defaults to be more reasonable

--- test/RunState.spec.js
+++ test/RunState.spec.js
@@ -19,8 +19,8 @@ describe('RunState', function() {
 
 
   it('should set default properties', function() {
-    expect(runState.iterations).toBe(0);
-    expect(runState.numSamples).toBe(20);
+    expect(runState.iterations).toBe(20);
+    expect(runState.numSamples).toBe(15);
     expect(runState.recentResult).toEqual({});
   });
 
@@ -28,7 +28,7 @@ describe('RunState', function() {
   describe('.setIterations()', function() {
     it('should set provided arguments to runState object', function() {
       runState.setIterations(15);
-      expect(runState.numSamples).toBe(20);
+      expect(runState.numSamples).toBe(15);
       expect(runState.iterations).toBe(15);
     });
   });
@@ -39,8 +39,8 @@ describe('RunState', function() {
       runState.numSamples = 50;
       runState.iterations = 99;
       runState.resetIterations();
-      expect(runState.numSamples).toBe(20);
-      expect(runState.iterations).toBe(0);
+      expect(runState.numSamples).toBe(5);
+      expect(runState.iterations).toBe(20);
     });
   });
 
