ee30d95a2ccd507d069028d6fbe1b48809147069
Jeff Cross <middlefloor@gmail.com>
2014-11-18 11:53:18 -0800

    feat(AutoRunner): implement auto-running functionality with logging

--- test/AutoRunner.spec.js
+++ test/AutoRunner.spec.js
@@ -2,17 +2,20 @@ var di = require('di');
 var testing = require('../node_modules/di/dist/cjs/testing');
 var AutoRunner = require('../lib/AutoRunner');
 var Globals = require('../lib/Globals');
+var Logger = require('../lib/Logger');
 var MockGlobals = require('./MockGlobals');
 var RunState = require('../lib/RunState');
+var Rx = require('rx');
 var Utils = require('../lib/Utils');
 
 describe('AutoRunner', function() {
-  var globals, autoRunner, utils;
+  var globals, autoRunner, logger, utils;
   beforeEach(function() {
     testing.use(MockGlobals).as(Globals);
-    testing.inject(AutoRunner, Globals, RunState, Utils, function(a,g,rs,u){
+    testing.inject(AutoRunner, Globals, Logger, RunState, Utils, function(a,g,l,rs,u){
       globals = g;
       autoRunner = a;
+      logger = l;
       runState = rs;
       utils = u;
     });
@@ -20,10 +23,10 @@ describe('AutoRunner', function() {
 
   describe('.bootstrap', function() {
     it('should call runState.setDefault with query params', function() {
-      var spy = spyOn(runState, 'setDefault');
+      spyOn(runState, 'setDefault');
       globals._window.location.search = '?numSamples=50&iterations=100';
       autoRunner.bootstrap();
-      expect(spy).toHaveBeenCalledWith({
+      expect(runState.setDefault).toHaveBeenCalledWith({
         numSamples: '50',
         iterations: '100'
       });
@@ -32,6 +35,54 @@ describe('AutoRunner', function() {
 
 
   describe('.ready()', function() {
+    it('should call runBenchmark with the current value of runState', function() {
+      spyOn(autoRunner, 'runBenchmark');
+      globals._window.setTimeout = function (fn) {
+        fn();
+      }
+      autoRunner.ready();
+      expect(autoRunner.runBenchmark.calls[0].args[0].iterations).toBe(20);
+      expect(autoRunner.runBenchmark.calls[0].args[0].numSamples).toBe(15);
+    });
+  });
 
+
+  describe('.runBenchmark()', function() {
+    var subscribeSpy;
+
+    beforeEach(function(){
+      subscribeSpy = jasmine.createSpy('subscribe');
+      spyOn(autoRunner, 'runAllTests').andReturn({
+        subscribe: subscribeSpy
+      });
+    });
+
+    it('should call runAllTests()', function() {
+      autoRunner.runBenchmark(this._runState);
+      expect(autoRunner.runAllTests).toHaveBeenCalled();
+    });
+
+
+    it('should subscribe to the observable', function() {
+      autoRunner.runBenchmark(this._runState);
+      expect(subscribeSpy).toHaveBeenCalled();
+    });
+
+
+    it('should log reports to Logger when subscription sends updates', function() {
+      spyOn(logger, 'write');
+      autoRunner.runBenchmark(this._runState);
+
+      //Send subscription message
+      subscribeSpy.calls[0].args[0]('new report');
+      expect(logger.write).toHaveBeenCalledWith('new report');
+    });
+  });
+
+
+  describe('.runAllTests()', function() {
+    it('should return an observable', function() {
+      expect(typeof autoRunner.runAllTests().subscribe).toBe('function');
+    });
   });
 });
