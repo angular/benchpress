9caa1d394e1b3bf1b69ef909f78972b204dd3a26
Jeff Cross <middlefloor@gmail.com>
2014-12-18 15:57:22 -0800

    refactor(benchmarksService): add _benchmarksCache object to service for easier testing

--- dashboard/components/benchmarks-service/benchmarks-service.js
+++ dashboard/components/benchmarks-service/benchmarks-service.js
@@ -1,15 +1,15 @@
 angular.module('bpdBenchmarksService', []).
   service('benchmarksService', ['$http', function($http) {
-    var selectedName, benchmarksCache;
+    var selectedName;
 
     this.get = function(options) {
-      if (options && options.cacheOk && benchmarksCache) {
-        return benchmarksCache;
+      if (options && options.cacheOk && this._benchmarksCache) {
+        return this._benchmarksCache;
       }
       return $http.get('/api/benchmarks').then(function(res) {
-        benchmarksCache = res.data;
-        return benchmarksCache;
-      });
+        this._benchmarksCache = res.data;
+        return this._benchmarksCache;
+      }.bind(this));
     };
 
     this.select = function (name) {
@@ -19,8 +19,8 @@ angular.module('bpdBenchmarksService', []).
     // By calculating at get-time, the object is guaranteed to be latest cached
     // version of benchmark
     this.selected = function () {
-      if (!benchmarksCache || !benchmarksCache.benchmarks) return;
-      var benchmarks = benchmarksCache.benchmarks;
+      if (!this._benchmarksCache || !this._benchmarksCache.benchmarks) return;
+      var benchmarks = this._benchmarksCache.benchmarks;
 
       for (i in benchmarks) {
         if (benchmarks[i] && benchmarks[i].name === selectedName) {
