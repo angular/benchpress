9c2e0c0d96f28179fa88cbed8f8fc2f0f2b4893b
Jeff Cross <middlefloor@gmail.com>
2014-12-17 16:11:59 -0800

    chore(dashboard): transition to bower for lib dependencies

--- dashboard/karma.conf.js
+++ dashboard/karma.conf.js
@@ -2,8 +2,9 @@ module.exports = function(config) {
   config.set({
     frameworks: ['jasmine'],
     files: [
-      'libs/angular.js',
-      'libs/*.js',
+      'bower_components/angular/angular.js',
+      'bower_components/angular-route/angular-route.js',
+      'bower_components/angular-mocks/angular-mocks.js',
       'app.js',
       'components/**/*',
       '*.js'
