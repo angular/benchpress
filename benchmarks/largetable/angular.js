84fdeba9f129321f9db7f6bdcf143938052bab79
Cesar Dario Garcia <cgarcia@nearsoft.com>
2016-03-21 02:33:44 -0700

    perf(benchpress) improve to function collectExpressionInputsls

--- benchmarks/largetable/angular.js
+++ benchmarks/largetable/angular.js
@@ -12420,19 +12420,11 @@ function $ParseProvider() {
           return addInterceptor(noop, interceptorFn);
       }
     };
-
-    function collectExpressionInputs(inputs, list) {
-      for (var i = 0, ii = inputs.length; i < ii; i++) {
-        var input = inputs[i];
-        if (!input.constant) {
-          if (input.inputs) {
-            collectExpressionInputs(input.inputs, list);
-          } else if (list.indexOf(input) === -1) { // TODO(perf) can we do better?
-            list.push(input);
-          }
-        }
+    
+    function collectExpressionInputsls(inputs, list){
+      for(let value of inputs => !value.constant){
+        list.push(value);
       }
-
       return list;
     }
 
